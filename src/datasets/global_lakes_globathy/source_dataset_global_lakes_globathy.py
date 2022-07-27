ecf79551610e7ba2bf143b379d105868cc0f7464
mmacferrin <michael.macferrin@colorado.edu>
2022-07-27 13:27:11 -0600

    Adding code for globathy lakes generation the world over.

--- src/datasets/global_lakes_globathy/source_dataset_global_lakes_globathy.py
+++ src/datasets/global_lakes_globathy/source_dataset_global_lakes_globathy.py
@@ -21,3 +21,26 @@ class source_dataset_global_lakes_globathy(etopo_source_dataset.ETOPO_source_dat
         """Initialize the global_lakes_globathy source dataset object."""
 
         super(source_dataset_global_lakes_globathy, self).__init__("global_lakes_globathy", configfile)
+
+    def create_global_lakes_globathy_tiles(self,
+                                           resolution_s = 15,
+                                           crm_only_if_1s = True,
+                                           verbose = True):
+
+        # 1) Get the tile outlines and resolutions of each grid to create. (Use the etopo empty tiles to do this.)
+        etopo_config = utils.configfile.config()
+        if resolution_s == 1:
+            etopo_gpkg_name = etopo_config.etopo_tile_geopackage_1s
+        elif resolution_s == 15:
+            etopo_gpkg_name = etopo_config.etopo_tile_geopackage_15s
+        elif resolution_s == 60:
+            etopo_gpkg_name = etopo_config.etopo_tile_geopackage_60s
+        else:
+            raise ValueError("Unhandled value for parameter 'resolution_s':", resolution_s)
+
+        # 2) Get the directory of the output files and filenames for each output
+
+
+        # 3) Generate all the output grids. Lakes everywhere!
+
+        # 4) Then think about generating the gebco outlines where they should exist.
