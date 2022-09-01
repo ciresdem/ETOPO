cc038ba407d6bce6ba791cb20afc211b9f022c7a
mmacferrin <michael.macferrin@colorado.edu>
2022-09-01 07:58:56 -0600

    Added two new layers, one for BedMachine Bed, the other for BedMachine Surface. These have different rankings, thus will be different datasets. The regular BedMachine layer is now set to "is_active = False"

---
+++ src/datasets/BedMachine_Surface/source_dataset_BedMachine_Surface.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+"""Source code for the BedMachine_Surface ETOPO source dataset class."""
+
+import os
+
+THIS_DIR = os.path.split(__file__)[0]
+
+##############################################################################
+# Code for importing the /src directory so that other modules can be accessed.
+import import_parent_dir
+import_parent_dir.import_src_dir_via_pythonpath()
+##############################################################################
+
+import datasets.etopo_source_dataset as etopo_source_dataset
+
+class source_dataset_BedMachine_Surface(etopo_source_dataset.ETOPO_source_dataset):
+    """Look in "src/datasets/etopo_source_dataset.py" to get base class definition."""
+    def __init__(self,
+                 configfile = os.path.join(THIS_DIR, "BedMachine_Surface_config.ini" )):
+        """Initialize the BedMachine_Surface source dataset object."""
+
+        super(source_dataset_BedMachine_Surface, self).__init__("BedMachine_Surface", configfile)
