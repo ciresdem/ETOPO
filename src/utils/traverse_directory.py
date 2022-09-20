4cc118dc5d53c83ba0252e4d188a1c4ab9714351
mmacferrin <michael.macferrin@colorado.edu>
2022-09-19 23:07:07 -0600

    Added a parameter to specify directory recursion search depth for files.

--- src/utils/traverse_directory.py
+++ src/utils/traverse_directory.py
@@ -6,8 +6,12 @@ import re
 import os
 import argparse
 
-def list_files(dirname, regex_match = r"\A\w*", ordered=True, include_base_directory = True):
-    file_list = _list_files_recurse(dirname, regex_match=regex_match)
+def list_files(dirname: str,
+               regex_match: str = r"\A\w*",
+               ordered: bool = True,
+               depth: int = -1,
+               include_base_directory: bool = True) -> list:
+    file_list = _list_files_recurse(dirname, regex_match=regex_match, depth=depth)
 
     if not include_base_directory:
         file_list = [fn[len(dirname):].lstrip(os.sep) for fn in file_list]
@@ -17,7 +21,7 @@ def list_files(dirname, regex_match = r"\A\w*", ordered=True, include_base_direc
 
     return file_list
 
-def _list_files_recurse(dirname, regex_match = None):
+def _list_files_recurse(dirname, regex_match = None, depth=-1):
     fpath_list = os.listdir(dirname)
     file_list = []
 
@@ -28,8 +32,8 @@ def _list_files_recurse(dirname, regex_match = None):
             print("dirname:", dirname)
             print("entryname:", entryname)
             raise e
-        if os.path.isdir(fpath):
-            file_list.extend(_list_files_recurse(fpath, regex_match=regex_match))
+        if (depth==-1 or depth > 0) and os.path.isdir(fpath):
+            file_list.extend(_list_files_recurse(fpath, regex_match=regex_match, depth=(-1 if (depth == -1) else (depth-1))))
         elif (regex_match is None) or (re.search(regex_match, entryname) != None):
             file_list.append(fpath)
     return file_list
@@ -37,7 +41,8 @@ def _list_files_recurse(dirname, regex_match = None):
 def define_and_parse_args():
     parser = argparse.ArgumentParser(description="A utility for recursively finding (or deleting) files in a directory and sub-directories.")
     parser.add_argument("DIR", type=str, default=os.getcwd(), help="Directory to search within. Default: Current working directory.")
-    parser.add_argument("-text", "-t", type=str, default=r"\A\w*", help="Regular expression to match.")
+    parser.add_argument("-text", "-t", type=str, default=r"\A[\.\-\w]*", help="Regular expression to match.")
+    parser.add_argument("-depth", type=int, default=-1, help="Maximum directory depth to search. -1 is no limit. 0 is only the local directory. 1 or more delves that many sub-directories. Default -1.")
     parser.add_argument("--delete", "-d", action="store_true", default=False, help="Delete the files matching the search query. NOTE: Suggest to call first without this option to see what will be deleted, then re-call with -d.")
 
     return parser.parse_args()
@@ -45,7 +50,8 @@ def define_and_parse_args():
 if __name__ == "__main__":
     args = define_and_parse_args()
     fnames = list_files(args.DIR,
-                        regex_match=args.text)
+                        regex_match=args.text,
+                        depth=args.depth)
     for fn in fnames:
         if args.delete:
             print("Removing", fn)
